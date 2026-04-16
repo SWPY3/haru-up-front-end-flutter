f163197f3afb51855e4670ac14d03f850f8d5fab
dahun_27 <hdh011027@gmail.com>
2026-04-16 20:43:25 +0900

    feat: 로그인 화면 UI 구현

--- lib/presentation/pages/login/login_screen.dart
+++ lib/presentation/pages/login/login_screen.dart
@@ -5,9 +5,48 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: Image.asset(
+              'assets/images/login/image_login_background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            bottom: 200,
+            left: 20,
+            right: 20,
+            child: Column(
+              children: [
+                // 카카오 로그인 버튼
+                GestureDetector(
+                  onTap: () {
+                    //TODO: 카카오 로그인 로직
+                  },
+                  child: Image.asset(
+                    'assets/images/login/kakao_login.png',
+                    height: 56,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                // 네이버 로그인 버튼
+                GestureDetector(
+                  onTap: () {
+                    //TODO: 네이버 로그인 로직
+                  },
+                  child: Image.asset(
+                    'assets/images/login/naver_login.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
